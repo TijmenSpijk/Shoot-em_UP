3387e41a2c06ae2e75c9ecf6efe5213225fb6c13
vincentlandes <vincentlandes@users.noreply.github.com>
2018-10-23 14:45:16 +0200

    vierkantje joe

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import Graphics.Gloss
+
+window :: Display
+window = InWindow "Shoot'em Up" (400, 400) (10, 10)
+
+background :: Color
+background = white
+
+drawing :: Picture
+drawing = circle 80
+
 main :: IO ()
-main = putStrLn "Fucking Vincent is altijd te laat en is zo fucking traag!"
+main = display window background drawing
+              
+
+
